f7f9473506809eec62b84f85d59584bb117e856f
skbolton <stephenkbolton@gmail.com>
2018-08-15 20:28:44 -0600

    add comment

--- src/routes/issues.js
+++ src/routes/issues.js
@@ -18,14 +18,8 @@ issuesRoutes.route('/')
   .post(async (req, res, next) => {
     try {
       const { title, ownerId, description } = req.body
-      const confirmUserExists = usersRepo.findById(ownerId)
-
-      confirmUserExists.catch(e => {
-        e.status = 400
-        throw e
-      })
-
-      const user = await confirmUserExists
+      // confirm the user exists
+      await usersRepo.findById(ownerId)
       const issue = await issuesRepo.create({ title, ownerId, description })
 
       return res.status(201).json({ issue })
